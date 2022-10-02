a072d27c45e6fded0b237538f6327057728ec5fe
SamuelAdamson <adamson.samuel10@gmail.com>
2022-10-02 00:26:06 -0600

    Merge sort added

--- sorts/mergeSort.cpp
+++ sorts/mergeSort.cpp
@@ -1,21 +1,59 @@
 #include <vector>
 using namespace std;
 
+/*
+    Implementation of merge sort
+    This is my go-to sorting algorithm, if required during 
+        a competition / interview
+*/
 
-vector<int> merge(vector<int>& v, int l1, int h1, int l2, int h2) {
 
+void merge(vector<int>& v, int l, int m, int r) {
+    // Create two subarrays to store values for merge
+    int i = 0, j = 0, k = l;
+    vector<int> left(m - l + 1), right(r - m);
+
+    // Populate vectors for merge
+    for(int i = l; i <= m; i++) left[i - l] = v[i];
+    for(int i = m + 1; i <= r; i++) right[i - (m + 1)] = v[i];
+
+    // Merge lists while values left in both right and left
+    while(i < left.size() && j < right.size()) {
+        if(left[i] <= right[j]) {
+            v[k] = left[i];
+            i++;
+        }
+        else {
+            v[k] = right[j];
+            j++;
+        }
+
+        // Next index in merged list
+        k++;
+    }
+
+    // Empty lists
+    while(i < left.size()) {
+        v[k] = left[i];
+        k++, i++;    
+    }
+    
+    while(j < right.size()) {
+        v[k] = right[j];
+        k++, j++;   
+    }
 }
 
-void mergeSort(vector<int>& v, int l, int h) {
-    int n = h - l + 1;
+void mergeSort(vector<int>& v, int l, int r) {
+    if(l < r) {
+        // Get mid point
+        int m = l + (r - l) / 2;
 
-    if(h - l > 1) {
-        int l1 = l, h1 = l + n / 2;
-        int l2 = l + n/2 + 1, h2 = h;
+        // Recursive call on each half
+        mergeSort(v, l, m);
+        mergeSort(v, m+1, r);
 
-        mergeSort(v, l1, h1);
-        mergeSort(v, l2, h2);
-
-        merge(v, l1, h1, l2, h2);
+        // Merge together lists
+        merge(v, l, m, r);
     }
 }
