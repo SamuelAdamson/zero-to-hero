2d3850f27ed5d78d76c679b4e0226127931f5f3e
SamuelAdamson <adamson.samuel10@gmail.com>
2023-04-10 20:59:11 -0400

    LC problem of day streak 2

---
+++ LC/stack/LC_2390/removingStarsFromString.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string st;
+
+        for(char c : s) {
+            if(c == '*') st.pop_back();
+            else st.push_back(c);
+        }
+
+        return st;
+    }
+};
